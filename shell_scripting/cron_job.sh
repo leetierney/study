cd2f6880b1d593814f1465a124e231aa4e5dd7d1
ljjtmm <86856014+ljjtmm@users.noreply.github.com>
2024-07-21 19:40:55 +0100

    created basic cron job

--- shell_scripting/cron_job.sh
+++ shell_scripting/cron_job.sh
@@ -6,8 +6,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Define the logfile path based on the script's directory
 LOGFILE="${SCRIPT_DIR}/logfile.log"
 
-# Define the cron job with the dynamic logfile path
-CRON_JOB="33 19 * * * echo \"Hello World\" >> ${LOGFILE}"
+# Define the cron job with the dynamic logfile path, set to run every minute
+CRON_JOB="* * * * * date '+\%Y-\%m-\%d \%H:\%M:\%S' >> ${LOGFILE}"
 
 # Check if the cron job already exists
 (crontab -l | grep -F "$CRON_JOB") || (crontab -l; echo "$CRON_JOB") | crontab -
