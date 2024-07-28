2351738053f9a82e5507d8451c15c77105cae0dc
ljjtmm <86856014+ljjtmm@users.noreply.github.com>
2024-07-28 22:05:08 +0100

    finish up ch2, start ch3

---
+++ ml/hands_on_ml/1_ml_landscape/ch2/2_p4.py
@@ -0,0 +1,126 @@
+import pandas as pd
+import numpy as np
+import os
+from scipy.stats import expon, reciprocal
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, FunctionTransformer, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.model_selection import GridSearchCV, StratifiedShuffleSplit, RandomizedSearchCV
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.compose import ColumnTransformer
+from sklearn.svm import SVR
+
+HOUSING_PATH = os.path.join("datasets", "housing")
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+housing = load_housing_data()
+
+
+housing["income_cat"] = pd.cut(housing["median_income"],
+                               bins = [0., 1.5, 3.0, 4.5, 6., np.inf],
+                               labels = [1, 2, 3, 4, 5])
+
+split = StratifiedShuffleSplit(n_splits = 1, test_size = 0.2, random_state = 42)
+
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+
+housing = strat_train_set.copy()
+
+housing = strat_train_set.drop("median_house_value", axis=1) # drop labels for training set
+housing_labels = strat_train_set["median_house_value"].copy()
+
+housing_num = housing.drop('ocean_proximity', axis=1)
+
+rooms_ix, bedrooms_ix, population_ix, household_ix = [
+    list(housing.columns).index(col)
+    for col in ("total_rooms", "total_bedrooms", "population", "households")]
+
+def add_extra_features(X, add_bedrooms_per_room = True):
+    rooms_per_household = X[:, rooms_ix] / X[:, household_ix]
+    population_per_household = X[:, population_ix] / X[:, household_ix]
+    if add_bedrooms_per_room:
+        bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+        return np.c_[X, rooms_per_household, population_per_household,
+                     bedrooms_per_room]
+    else:
+        return np.c_[X, rooms_per_household, population_per_household]
+
+num_pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy = "median")),
+        ('attribs_adder', FunctionTransformer(add_extra_features, validate = False)),
+        ('std_scaler', StandardScaler()),
+    ])
+
+num_attribs = list(housing_num)
+cat_attribs = ["ocean_proximity"]
+
+full_pipeline = ColumnTransformer([
+        ("num", num_pipeline, num_attribs),
+        ("cat", OneHotEncoder(), cat_attribs),
+    ])
+
+housing_prepared = full_pipeline.fit_transform(housing)
+
+param_grid = [
+    # try 12 (3×4) combinations of hyperparameters
+    {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+    # then try 6 (2×3) combinations with bootstrap set as False
+    {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
+  ]
+
+forest_reg = RandomForestRegressor(random_state = 42)
+
+# train across 5 folds, that's a total of (12+6)*5=90 rounds of training 
+grid_search = GridSearchCV(forest_reg, param_grid, cv = 5, scoring = 'neg_mean_squared_error', return_train_score = True)
+grid_search.fit(housing_prepared, housing_labels)
+
+
+feature_importances = grid_search.best_estimator_.feature_importances_
+
+param_distribs = {
+        'kernel': ['linear', 'rbf'],
+        'C': reciprocal(20, 200000),
+        'gamma': expon(scale=1.0),
+    }
+
+svm_reg = SVR()
+rnd_search = RandomizedSearchCV(svm_reg, param_distributions = param_distribs,
+                                n_iter = 50, cv = 5, scoring = 'neg_mean_squared_error',
+                                verbose = 2, n_jobs = 4, random_state = 42)
+
+rnd_search.fit(housing_prepared, housing_labels)
+
+def indices_of_top_k(arr, k):
+    return np.sort(np.argpartition(np.array(arr), -k)[-k:])
+
+class TopFeatureSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, feature_importances, k):
+        self.feature_importances = feature_importances
+        self.k = k
+    def fit(self, X, y=None):
+        self.feature_indices_ = indices_of_top_k(self.feature_importances, self.k)
+        return self
+    def transform(self, X):
+        return X[:, self.feature_indices_]
+
+k = 5
+
+prepare_select_and_predict_pipeline = Pipeline([
+    ('preparation', full_pipeline),
+    ('feature_selection', TopFeatureSelector(feature_importances, k)),
+    ('svm_reg', SVR(**rnd_search.best_params_))
+])
+
+prepare_select_and_predict_pipeline.fit(housing, housing_labels)
+some_data = housing.iloc[:4]
+some_labels = housing_labels.iloc[:4]
+
+print("Predictions:\t", prepare_select_and_predict_pipeline.predict(some_data))
+print("Labels:\t\t", list(some_labels))
